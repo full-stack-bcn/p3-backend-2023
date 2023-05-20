aa1f15437d53aeac59f0ec49a847dae6536bffcd
Angel Diaz <warydiaz@hotmail.com>
2023-05-20 14:05:13 +0200

    adding seed.ts file and seed to package.json

---
+++ src/seed.ts
@@ -0,0 +1,41 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const cyberbingo = await prisma.partner.create({
+  data: {
+    name: "Cyberbingo",
+    url: "www.cyberbingo.com",
+  },
+});
+
+console.log(`created partner: ${cyberbingo.name}`);
+
+const vegascrest = await prisma.partner.create({
+  data: {
+    name: "Vegascrest",
+    url: "www.vegascrest.ag",
+  },
+});
+
+console.log(`created partner: ${vegascrest.name}`);
+
+const welcomeBonuses = await prisma.promotion.create({
+  data: {
+    name: "Welcome Bonus",
+    url: "welcome-bonus",
+    partnerId: cyberbingo.id,
+  },
+});
+
+console.log(`created promotion: ${welcomeBonuses.name}`);
+
+const animalKingdom = await prisma.promotion.create({
+  data: {
+    name: "The Animal Kingdom Tourney",
+    url: "the-animal-kingdom-tourney",
+    partnerId: vegascrest.id,
+  },
+});
+
+console.log(`created promotion: ${animalKingdom.name}`);
