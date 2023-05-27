1f8b6c84fdd059718a51f4652883b097401fc0b6
Angel Diaz <warydiaz@hotmail.com>
2023-05-27 22:24:14 +0200

    Adding more data to seed file. updating Tournament UML

--- src/seed.ts
+++ src/seed.ts
@@ -1,6 +1,4 @@
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
+import prisma from "./prisma-client.js";
 
 const cyberbingo = await prisma.partner.create({
   data: {
@@ -44,11 +42,78 @@ const futureDate = new Date();
 futureDate.setMonth(futureDate.getMonth() + 1);
 
 const tournament = await prisma.tournament.create({
-  data:{
+  data: {
     promotionId: animalKingdom.id,
     startDate: new Date(),
-    endDate: futureDate
-  }
-})
+    endDate: futureDate,
+  },
+});
 
-console.log(`created tournament: ${tournament.id} of the promotion ${animalKingdom.name}`);
+console.log(
+  `created tournament: ${tournament.id} of the promotion ${animalKingdom.name}`
+);
+
+const tournamentWelcomeBonuses = await prisma.tournament.create({
+  data: {
+    promotionId: welcomeBonuses.id,
+    startDate: new Date(),
+    endDate: futureDate,
+  },
+});
+
+console.log(
+  `created tournament: ${tournamentWelcomeBonuses.id} of the promotion ${welcomeBonuses.name}`
+);
+
+const ranking = await prisma.ranking.create({
+  data: {
+    tournamentId: tournament.id,
+    startDate: new Date(),
+    endDate: futureDate,
+  },
+});
+
+console.log(`created ranking: ${ranking.id} of the promotion ${tournament.id}`);
+
+const rankingWelcomeBonuses = await prisma.ranking.create({
+  data: {
+    tournamentId: tournamentWelcomeBonuses.id,
+    startDate: new Date(),
+    endDate: futureDate,
+  },
+});
+
+console.log(
+  `created ranking: ${rankingWelcomeBonuses.id} of the promotion ${tournamentWelcomeBonuses.id}`
+);
+
+const position = await prisma.position.create({
+  data: {
+    rankingId: ranking.id,
+    position: 1,
+    userName: "the_fisrt_one",
+    gamesWon: 150,
+    prize: 500,
+    partnerId: cyberbingo.id,
+  },
+});
+
+console.log(
+  `created position: ${position.id} of the promotion ${ranking.id}`
+);
+
+const secondPosition = await prisma.position.create({
+  data: {
+    rankingId: ranking.id,
+    position: 2,
+    userName: "the_second",
+    gamesWon: 130,
+    prize: 50,
+    partnerId: cyberbingo.id,
+  },
+});
+
+console.log(
+  `created position: ${secondPosition.id} of the promotion ${ranking.id}`
+);
+
